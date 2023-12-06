1a5b728a160d5c73d160f9e0efb0e39308c33d0d
Louis <louisstein16@gmail.com>
2023-12-06 14:34:23 +0100

    Add structs and update contract balance calculation

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -2,13 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-// Channel
-
-
-
-
-// FlashLoan
-
+// Interfaces
 interface FlashBorrower {
     function onFlashLoan(
         address initiator,
@@ -18,7 +12,58 @@ interface FlashBorrower {
     ) external returns (bytes32);
 }
 
+// Stucts
+
+struct Participant {
+    address addresse;
+}
+
+struct Channel_State {
+    int channel_id;
+    int balance;
+    int balance_A;
+    int balance_B;
+    int version_num;
+    bool finalized_a;
+    bool finalized_b;
+}
+
+struct Channel_Params{
+    Participant participant_a;
+    Participant participant_b;
+}
+
+struct Channel_Control{
+    bool funded_a;
+    bool funded_b;
+}
+
+struct Channel{
+    Channel_State state;
+    Channel_Params params;
+    Channel_Control control;
+}
+
 contract FlashLoan {
+    // Variables
+    uint256 public Contract_Balance = 0 ether;
+
+    // 
+    mapping(int => uint256) public balances;
+
+    // Channel
+
+
+    // FlashLoan
+
+    // Update Contract_Balance with the amount of ETH in every channel
+    function updateContractBalance() public returns (bool) {
+        // Your code here
+        return true;
+    }
+     
+     
+
 
     function flashLoan(
     FlashBorrower receiver,
