54856d614d6cc36109d0e1e1b88cafb1dea6858c
timostra <timo.strassburger@gmail.com>
2023-12-15 17:05:58 +0100

    Pay entfernt

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -154,50 +154,6 @@ contract FlashLoan {
         }
     }
 
-    /**
-     * @dev Pays the given amount from the balance of the caller to the other participant
-     *      and updates the balance of either participant_a or participant_b depending on the caller 
-     *      sets finalized_a and finalized_b to false and increases the version_num by 1
-     * @param channel_id The id of the channel
-     * @param caller The address of the caller
-     * @param amount The amount to pay
-     */
-    function pay(int channel_id, address caller, uint256 amount) public {
-        //Bool to know if caller is A or B
-        bool callerIsA=false;
-        
-        //Check if Channel exists
-        require(channels[channel_id].state.channel_id == channel_id, "Channel does not exist");
-
-        //Check if Caller is part of the given Channel
-        //TODO == funktioniert nicht für Typ address
-        require(channels[channel_id].params.participant_a.addresse == caller || channels[channel_id].params.participant_b.addresse == caller, "Caller is not part of the given Channel");
-
-        //Define if Caller is A or B 
-        if(channels[channel_id].params.participant_b.addresse == caller) callerIsA = true;
-        
-        //Check if Caller has enough Money, if True Transaktion is carried out
-        if(callerIsA){
-            require(channels[channel_id].state.balance_A >= amount, "Balance in Channel is not enough");
-
-            channels[channel_id].state.balance_A -= amount;
-            channels[channel_id].state.balance_B += amount; 
-
-        }
-        else{
-            require(channels[channel_id].state.balance_B >= amount, "Balance in Channel is not enough");
-
-            channels[channel_id].state.balance_B -= amount;
-            channels[channel_id].state.balance_A += amount; 
-        }
-
-        //Finalized is false, because State of Channel has changed 
-        channels[channel_id].state.finalized_a = false;
-        channels[channel_id].state.finalized_b = false;
-
-        //Increase of Version Number 
-        channels[channel_id].state.version_num ++;
-    }
 
       // Update Contract_Balance with the amount
     function updateContractBalance(int channel_id) public {
