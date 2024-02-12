a858f2d345f1e91ab305b43b0dcbca690947e739
Louis <louisstein16@gmail.com>
2024-02-12 17:42:49 +0100

    Refactor buttonLayout.js and buttonLayout.module.css

--- demo-website/src/app/[components]/[buttonLayout]/buttonLayout.js
+++ demo-website/src/app/[components]/[buttonLayout]/buttonLayout.js
@@ -1,11 +1,11 @@
 import style from "./buttonLayout.module.css";
 
-const ButtonLayout = () => (
+const ButtonLayout = ({i1,i2,i3,i4}) => (
   <div className={style.ButtonLayout}>
-        <div className={style.button}>Open</div>
-        <div className={style.button}>Fund</div>
-        <div className={style.button}>Close</div>
-        <div className={style.button}>Withdraw</div>
+    <div className={`${style.button} ${i1 ? style.inactive : ""}`}>Open</div>
+    <div className={`${style.button} ${i2 ? style.inactive : ""}`}>Fund</div>
+    <div className={`${style.button} ${i3 ? style.inactive : ""}`}>Close</div>
+    <div className={`${style.button} ${i4 ? style.inactive : ""}`}>Withdraw</div>
   </div>
 );
 
--- demo-website/src/app/[components]/[rightPanel]/topRightPanel.js
+++ demo-website/src/app/[components]/[rightPanel]/topRightPanel.js
@@ -3,9 +3,9 @@ import strucStyle from "./../../styles.module.css";
 import style from "./topRightPanel.module.css";
 
 const TopRightPanel = () => (
-    <div className={strucStyle.TopRightPanel}>
-          <ButtonLayout />
-    </div>
+  <div className={strucStyle.TopRightPanel}>
+    <ButtonLayout i1={false} i2={false} i3={false} i4={true}/>
+  </div>
 );
 
 export default TopRightPanel;
