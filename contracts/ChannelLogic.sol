f7f9fed8158b2090434d1ec4fcb5bbeca50c9d60
Louis <louisstein16@gmail.com>
2024-02-19 13:26:53 +0100

    update

--- contracts/ChannelLogic.sol
+++ contracts/ChannelLogic.sol
@@ -7,11 +7,11 @@ import "hardhat/console.sol";
 interface FlashBorrower {
     function onFlashLoan(
         address initiator,
-        int256 amount,
-        int256 fee,
-        bytes calldata data
+        uint256 amount,
+        uint256 fee
     ) external returns (bool);
 }
+
 
 // Stuct Section
 
@@ -28,7 +28,6 @@ struct Channel_State {
     uint balance_B;
     int version_num;
     bool finalized;
-    bool closed;
 }
 
 // Define Channel Params with Participants
@@ -43,6 +42,8 @@ struct Channel_Control{
     bool funded_b;
     bool withdrawed_a;
     bool withdrawed_b;
+    uint sum_of_balances;
+    bool closed;
 }
 
 // Define Channel
@@ -56,26 +57,39 @@ struct Channel{
 
 contract ChannelLogic {
     // Constants
-    int256 flashLoanFee = 1; // 10% fee
+    uint256 flashLoanFee = 1; //  1 => 0.01% fee
 
     // Variables
-    uint256 public Contract_Balance = address(this).balance;
+    //uint256 public contract_pool = 0 ether; //kann maybe durch address(this).balance ersetzt werden
 
     Participant[] public participants;
     
     int public channel_count = 0;
+    int r = 0;
 
     //Events
-    event ChannelOpen(Channel channel);
+    event ChannelOpen();
     event ChannelFund();
     event ChannelWithdraw();
     event ChannelClose();
-
+    event ContractBalanceUpdated(uint256 newBalance);
+   
     // Mapping: Channel_ID => Channel
     mapping(int => Channel) public channels;
 
-    // Mapping: Channel_ID => Balance
-    mapping(int => uint256) public balances;
+    /*
+    // Mapping: User_Address => Balance, keeps track which address has funded which amount
+    mapping(address => uint256) public balances;
+
+    // Dynamic Array: Stores the addresses of accounts that currently have at least one active channel funded
+    address[] shareHolders;
+
+    // Mapping: Address => hasAlreadyFundedAChannel, keeps track if the address has already funded at least one active channel
+    mapping(address => uint256) public alreadyShareholding;
+    */
+
+    // Dynamic Array: Keeps track which channels are currently active
+    int[] activeChannels;
 
     // Compare two Participants
     function compareParticipants(Participant memory a, Participant memory b) private pure returns (bool) {
@@ -84,11 +98,13 @@ contract ChannelLogic {
 
     // Channel Section
 
+
     /**
-     * @dev Opens a new channel between two participants 
+     * Opens a new channel between two participants 
      *      and adds it to the channels mapping and updates the channel_count
      * @param params The parameters of the channel
      * @param state The state of the channel
+     * @dev Louis
      */
     function open(Channel_Params calldata params, Channel_State calldata state) public{
 
@@ -110,23 +126,28 @@ contract ChannelLogic {
         channel.params = params;
         channel.control.funded_a = false;
         channel.control.funded_b = false;
+        channel.control.withdrawed_a = false;
+        channel.control.withdrawed_b = false;
+        channel.control.closed = false;
+        channel.control.sum_of_balances = 0;
 
         // Increase channel_count
         channel_count++;
 
         // Add Channel to channels
         channels[state.channel_id] = channel;
+        activeChannels.push(state.channel_id);
 
         // Emit event
-        emit ChannelOpen(channel);
+        emit ChannelOpen();
     }
 
     receive() external payable {
-        Contract_Balance += msg.value;
+            emit ContractBalanceUpdated(msg.value);
     }
-    
+
     /**
-     * @dev Funds a channel with the given amount
+     * Funds a channel with the given amount
      *      and updates the balance of either participant_a or participant_b depending on the caller 
      * @param channel_id The id of the channel
      * @dev Oli Moritz Louis
@@ -164,62 +185,82 @@ contract ChannelLogic {
 
             // Log for tests, delete later
             console.log("Participant A has successfully funded the channel with ", channel.state.balance_A);
+                    emit ChannelFund();
         } else {
             channel.state.balance_B = amount;
             channel.control.funded_b = true;
 
             // Log for tests, delete later
             console.log("Participant B has successfully funded the channel with ", channel.state.balance_B);
+                    emit ChannelFund();
         }
+
+        /*
+        balances[msg.sender] += amount;
+        if(alreadyShareholding[msg.sender] == 0){
+            shareHolders.push(msg.sender);
+        }
+        alreadyShareholding[msg.sender] += 1;
+        */
 
         // Log for tests, delete later
         console.log("Their new balance is: ", address(msg.sender).balance);
-        console.log("The contract balance is: ", address(this).balance);
-        emit ChannelFund();
     }
+
+
     
     /**
-    * @dev Closes the channel and pays out the balance of the caller
-    * @param channel_id The id of the channel
+    * Closes the channel and pays out the balance of the caller
+    * @param finalState The final state in which the payout should happen, containing the balances of the participants
     * @dev Moritz
     */
-   function close(int channel_id) public {
-        Channel storage channel = channels[channel_id];
+   function close(Channel_State calldata finalState) public {
+        Channel memory channel = channels[finalState.channel_id];
         // Check if channel exists
-        require(channel.state.channel_id == channel_id, "Channel does not exist");
+        require(channel.state.channel_id == finalState.channel_id, "Channel does not exist");
 
         // Check if Caller is part of the given Channel
         require(channel.params.participant_a.addresse == msg.sender || channel.params.participant_b.addresse == msg.sender,
             "Sender is not a participant of the given channel");
 
-        // Check whether the channel has been finalized
-        require(channel.state.finalized, "Channel is not yet finalised");
+        // Check whether the sent state has been finalized
+        require(finalState.finalized, "Channel is not yet finalised");
 
         // Check whether the channel has been closed already
-        require(!channel.state.closed, "Channel has already been closed, use withdraw instead");
+        require(!channel.control.closed, "Channel has already been closed, use withdraw instead");
+
+        //Check whether the sum of the balances in the final state matches the sum of the balances in the control struct
+        require(finalState.balance_A + finalState.balance_B == channel.control.sum_of_balances, "The sum of the balances does not match the sum of the balances in the control struct");
+
+        // update the final state (needed in withdraw)
+        channel.state = finalState;
+   
+        //Close Channel
+        channel.control.closed = true;
 
         // Determine the participant and the corresponding balance
         bool senderIsA = msg.sender == channel.params.participant_a.addresse;
         uint256 amountToTransfer = senderIsA ? channel.state.balance_A : channel.state.balance_B;
 
-        //Close Channel
-        channel.state.closed = true;
 
         // Check if there is a balance to transfer and do the transfer
         if(amountToTransfer > 0) {
             if(senderIsA) {
-                channel.state.balance_A = 0;
+                require(!channel.control.withdrawed_a, "Sender has already withdrawn their balance");
+                channel.control.withdrawed_a = true;
                 (bool transferSuccess, bytes memory data) = payable(msg.sender).call{value: amountToTransfer}("");
                 require(transferSuccess, "Transfer failed");
             } else {
-                channel.state.balance_B = 0;
+                require(!channel.control.withdrawed_b, "Sender has already withdrawn their balance");
+                channel.control.withdrawed_b = true;
                 (bool transferSuccess, bytes memory data) = payable(msg.sender).call{value: amountToTransfer}("");
                 require(transferSuccess, "Transfer failed");
             }
         }
+        emit ChannelClose();
     }
     /**
-    * @dev Withdraws the balance of the caller if the channel has already been closed
+    * Withdraws the balance of the caller if the channel has already been closed
     * @param channel_id The id of the channel
     * @dev Moritz
      */
@@ -233,7 +274,7 @@ contract ChannelLogic {
             "Sender is not a participant of the given channel");
 
         // Check whether the channel has been closed already
-        require(channel.state.closed, "Channel has not been closed yet, use close instead");
+        require(channel.control.closed, "Channel has not been closed yet, use close instead");
 
         // Determine the participant and the corresponding balance
         bool senderIsA = msg.sender == channel.params.participant_a.addresse;
@@ -244,83 +285,114 @@ contract ChannelLogic {
 
         // and do the transfer
         if(senderIsA) {
-            channel.state.balance_A = 0;
+            require(!channel.control.withdrawed_a, "Sender has already withdrawn their balance");
+            channel.control.withdrawed_a = true;
             (bool transferSuccess, bytes memory data) = payable(msg.sender).call{value: amountToTransfer}("");
             require(transferSuccess, "Transfer failed");
         } else {
-            channel.state.balance_B = 0;
+            require(!channel.control.withdrawed_b, "Sender has already withdrawn their balance");
+            channel.control.withdrawed_b = true;
             (bool transferSuccess, bytes memory data) = payable(msg.sender).call{value: amountToTransfer}("");
             require(transferSuccess, "Transfer failed");
         }
+        emit ChannelWithdraw();
     }
 
-    //Calling this means that you are d'accord with how the trade went and are okay with ending the trade here
-    function finalize(Channel_State calldata newState) public {
-        //Einfache Implementation dass es erstmal läuft aber keine überprüffung der Signaturen 
-
-        // Check if channel exists
+    
+    /*
+    * Updates the channel with the given state in case of a dispute
+    * @param newState The new state of the channel
+    * @dev Moritz
+     */
+     /*
+    function disputeUpdate(Channel_State calldata newState) public {
+        //Check if channel exists
         require(channels[newState.channel_id].state.channel_id == newState.channel_id, "Channel does not exist");
+        //Check if participant is a paticipant of the channel
+        require(channels[newState.channel_id].params.participant_a.addresse == msg.sender || channels[newState.channel_id].params.participant_b.addresse == msg.sender, "Sender is not a participant of the given channel");
         //Check if channel is not finalized
         require(channels[newState.channel_id].state.finalized == false, "Channel is already finalized");
-        //Check if new Channel is finalized
-        require(newState.finalized == true, "New Channel is not finalized");
         //Check if Version Number is increased
         require(newState.version_num > channels[newState.channel_id].state.version_num, "Version Number is not increased");
-        
-        //Hier müsste dann die Überprüfung der Signaturen stattfinden
+
+        //TODO if used: Check if both parties signed this new state
 
         // Set new state
         channels[newState.channel_id].state = newState;
-
-        //Ideen wie man die Signautren überprüfen kann
-        //Video hilfreich: https://www.youtube.com/watch?v=ZcmQ92vBLgg
-        
-        //Idee1
-        /*
-        bytes32 hashedState = keccak256(abi.encode(newState));
-        // Check if channel exists
-        require(channels[newState.channel_id].state.channel_id == newState.channel_id, "Channel does not exist");
-
-        // Check if newState is signed by both participants
-        //TODO check if sigA and sigB are correct
-        require(ecrecover(hashedState, uint8(sigA[0]), bytes32(sigA[1]), bytes32(sigA[2])) == channels[newState.channel_id].params.participant_a.pubKey, "Signature of participant A is not valid");
-        require(ecrecover(hashedState, uint8(sigB[0]), bytes32(sigB[1]), bytes32(sigB[2])) == channels[newState.channel_id].params.participant_b.pubKey, "Signature of participant B is not valid");
-
-        // Set new state
-        channels[newState.channel_id].state = newState;
-
-        */
-
-        //Idee2
-        /*
-         // Check if channel exists
-        require(channels[newState.channel_id].state.channel_id == newState.channel_id, "Channel does not exist");
-
-        // Check if newState is signed by both participants
-        require(verifySig(hashedState, sigA, channels[newState.channel_id].params.participant_a.pubKey), "Signature of participant_a is not valid");
-        require(verifySig(hashedState, sigB, channels[newState.channel_id].params.participant_b.pubKey), "Signature of participant_b is not valid");
-
-        // Set new state
-        channels[newState.channel_id].state = newState;
-
-        */
     }
-
-
-    // FlashLoan Section
+    */
     
+
+      // FlashLoan Section
+
+
+    uint256 expected;
+    bool payedBack;
+
     function flashLoan(
     FlashBorrower receiver,
-    uint256 amount,
-    bytes calldata data) external returns (bool) {
-        
-        //uint256 fee = amount * flashLoanFee;
+    uint256 amount) external returns (bool) {
 
-        
+        require(amount <= _maxFlashLoan(), "Amount exceeds the flash loan limit, try to choose a smaller amount");
 
-        //bool success = receiver.onFlashLoan(msg.sender, amount, fee, data); // Execute the FlashLoan
-        //require(success, "FlashLoan failed");
+        //initialize variables
+        uint256 old = address(this).balance;
+        payedBack = false;
+
+        //pay the flashloan
+        (bool transferSuccess, bytes memory data) = payable(msg.sender).call{value: amount}("");
+        expected = amount + _flashFee(amount);
+
+        //execute onFlashLoan function on borrower side
+        receiver.onFlashLoan(msg.sender, amount, _flashFee(amount));
+
+        require(payedBack, "Payback did not happen or failed");
+
+        channelDistributor(_flashFee(amount));
 
         return true;
     }
+
+    function channelDistributor(
+        uint256 fees
+    ) internal  {
+        for(uint256 i = 0; i < activeChannels.length; i++) {
+            //get the channel via the mapping and the array
+            Channel memory c = channels[activeChannels[i]];
+
+            //distribute the fees to the participants and increase version number
+            c.control.sum_of_balances += c.control.sum_of_balances / address(this).balance * fees;
+        }
+    }
+
+    function payBack() external payable returns (bool) {
+        require(msg.value == expected, "Did not send sufficient funds to pay back the flash loan");
+        payedBack = true;
+        return true;
+    }
+
+    //external and internal functions to get the max flash loan amount and the fee for each loan
+
+    function maxFlashLoan(
+    ) external view returns (uint256) {
+        return _maxFlashLoan();
+    }
+
+    function _maxFlashLoan(
+    ) internal view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function flashFee(
+        uint256 amount
+    ) external view returns (uint256) {
+        return _flashFee(amount);
+    }
+
+    function _flashFee(
+        uint256 amount
+    ) internal view returns (uint256) {
+        return amount * flashLoanFee / 10000;
+    }
+
 }
